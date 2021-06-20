7ed9305c7ca135acfe59d3d41cf93eb5a0706a29
LBW <280986627@qq.com>
2021-06-20 21:02:41 +0800

    测试提交Git--firsttime

---
+++ src/com/lbw/test/Test.java
@@ -0,0 +1,12 @@
+package com.lbw.test;
+
+/**
+ * @author LBW
+ * @name
+ * @create 2021-06-18 1:15
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
